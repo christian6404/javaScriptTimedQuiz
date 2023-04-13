318d83e659ef281116d074bcc255443fb1406fc0
christian6404 <christian6404@gmail.com>
2023-04-13 14:43:10 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -68,10 +68,10 @@ if (window.location.href.includes("index.html")) { // entire Q&A page is in a co
         document.getElementById('questiontitle').style.visibility = 'hidden';
         document.getElementById('timer').style.visibility = 'hidden';
     }
-    if (window.location.href.includes("index.html")) {
+    if (window.location.href === 'https://christian6404.github.io/javaScriptTimedQuiz/' || window.location.href.includes('index.html')) {
         hidestuff()
     }
-
+    console.log(window.location)
     button.addEventListener('click', function questionsAndAnswers1() { // listens for a event on the start button and runs the start of the quiz
 
         function updateTime() {
